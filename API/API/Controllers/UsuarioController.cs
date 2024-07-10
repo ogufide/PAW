26981c73eaba0f5ed52e3446d86800d3621334a5
ogufide <oguevara80455@ufide.ac.cr>
2024-07-09 19:53:18 -0600

    Backend persona 2

--- API/API/Controllers/UsuarioController.cs
+++ API/API/Controllers/UsuarioController.cs
@@ -1,24 +1,30 @@
-﻿using Dapper;
-using API.Entities;
+﻿using API.Entities;
+using Dapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
 using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsuarioController : ControllerBase
+    public class UsuarioController(IConfiguration iConfiguration) : ControllerBase
     {
+        [AllowAnonymous]
         [HttpPost]
-        [Route("RegistrarUsuario")]
-        public async Task<IActionResult> RegistrarUsuario(Usuario ent)
+        [Route("CreateUsuario")]
+        public async Task<IActionResult> CreateUsuario(Usuario ent)
         {
             Respuesta resp = new Respuesta();
 
-            using (var context = new SqlConnection("Server=localhost;Database=PAW;Trusted_Connection=True;TrustServerCertificate=True;"))
+            using (var context = new SqlConnection(iConfiguration.GetSection("ConnectionStrings:DefaultConnection").Value))
             {
-                var result = await context.ExecuteAsync("RegistrarUsuario", new { ent.Identificacion, ent.Nombre, ent.Correo, ent.Contrasenna }, commandType: CommandType.StoredProcedure);
+                var result = await context.ExecuteAsync("CreateUsuario", new { ent.Identificacion, ent.Nombre, ent.Correo, ent.Contrasenna }, commandType: CommandType.StoredProcedure);
 
                 if (result > 0)
                 {
@@ -37,17 +43,18 @@ namespace API.Controllers
             }
         }
 
+        [AllowAnonymous]
         [HttpPost]
         [Route("IniciarSesion")]
         public async Task<IActionResult> IniciarSesion(Usuario ent)
         {
             Respuesta resp = new Respuesta();
 
-            using (var context = new SqlConnection("Server=localhost;Database=PAW;Trusted_Connection=True;TrustServerCertificate=True;"))
+            using (var context = new SqlConnection(iConfiguration.GetSection("ConnectionStrings:DefaultConnection").Value))
             {
-                var result = await context.QueryAsync<Usuario>("IniciarSesion", new { ent.Correo, ent.Contrasenna }, commandType: CommandType.StoredProcedure);
+                var result = await context.QueryFirstAsync<Usuario>("IniciarSesion", new { ent.Correo, ent.Contrasenna }, commandType: CommandType.StoredProcedure);
 
-                if (result.Count() > 0)
+                if (result != null)
                 {
                     resp.Codigo = 1;
                     resp.Mensaje = "OK";
@@ -63,5 +70,51 @@ namespace API.Controllers
                 }
             }
         }
+
+        [Authorize]
+        [HttpGet]
+        [Route("ReadUsuarios")]
+        public async Task<IActionResult> ReadUsuarios()
+        {
+            Respuesta resp = new Respuesta();
+
+            using (var context = new SqlConnection(iConfiguration.GetSection("ConnectionStrings:DefaultConnection").Value))
+            {
+                var result = await context.QueryAsync<Usuario>("ReadUsuarios", new { }, commandType: CommandType.StoredProcedure);
+
+                if (result.Count() > 0)
+                {
+                    resp.Codigo = 1;
+                    resp.Mensaje = "OK";
+                    resp.Contenido = result;
+                    return Ok(resp);
+                }
+                else
+                {
+                    resp.Codigo = 0;
+                    resp.Mensaje = "No hay usuarios registrados en este momento";
+                    resp.Contenido = false;
+                    return Ok(resp);
+                }
+            }
+        }
+
+        private string GenerarToken(int Consecutivo)
+        {
+            string SecretKey = iConfiguration.GetSection("Llaves:SecretKey").Value!;
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, Consecutivo.ToString()));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(10),
+                signingCredentials: cred);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
     }
 }

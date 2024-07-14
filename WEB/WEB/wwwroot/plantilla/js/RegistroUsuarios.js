704098183d78df3154000545b072e364acd38cba
ogufide <oguevara80455@ufide.ac.cr>
2024-07-14 01:51:38 -0600

    Signup seguro

---
+++ WEB/WEB/wwwroot/plantilla/js/RegistroUsuarios.js
@@ -0,0 +1,13 @@
+﻿<script>
+    function ConsultarNombre() {
+        let identificacion = $("#Identificacion").val();
+    $.ajax({
+        url: "https://apis.gometa.org/cedulas/" + identificacion,
+    method: "GET",
+    datatype: "json",
+    success: function (response) {
+        $("#Nombre").val(response.nombre);
+            }
+        });
+    }
+</script>

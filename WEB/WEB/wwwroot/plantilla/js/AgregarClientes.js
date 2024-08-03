64f25d537a6b689b109261bf8ec10cb61ec6fe2e
Echavac <edwinchavarriac@gmail.com>
2024-08-02 18:55:39 -0600

    Create AgregarClientes.js

---
+++ WEB/WEB/wwwroot/plantilla/js/AgregarClientes.js
@@ -0,0 +1,11 @@
+﻿function ConsultarNombre() {
+    let identificacion = jQuery("#Nombre").val();
+    jQuery.ajax({
+        url: "https://apis.gometa.org/cedulas/" + nombre,
+        method: "GET",
+        datatype: "json",
+        success: function (response) {
+            jQuery("#Nombre").val(response.nombre);
+        }
+    });
+}

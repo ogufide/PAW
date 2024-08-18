79c505ed94e9356b8af22ae98d5aed22e62f097d
ogufide <oguevara80455@ufide.ac.cr>
2024-08-18 15:32:39 -0600

    1

--- WEB/WEB/Program.cs
+++ WEB/WEB/Program.cs
@@ -17,11 +17,8 @@ builder.Services.AddScoped<IEmpleadosModel, EmpleadosModel>();
 builder.Services.AddScoped<IGimnasiosModel, GimnasiosModel>();
 builder.Services.AddScoped<IInscripcionClaseModel, InscripcionClaseModel>();
 builder.Services.AddScoped<IMembresiaModel, MembresiaModel>();
-<<<<<<< HEAD
 builder.Services.AddScoped<IProvinciasModel, ProvinciasModel>();
-=======
 builder.Services.AddScoped<IProductoModel, ProductoModel>();
->>>>>>> 84d04fff914028de4d51304a2b3aadfcf1f24d9c
 
 
 var app = builder.Build();

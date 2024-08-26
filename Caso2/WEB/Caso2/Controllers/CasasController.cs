9f4f255df63c51c9a9848e1d450c43735a763bbc
mari <maripazchg@hotmail.com>
2024-08-26 16:06:25 -0600

    caso2 creacion de base y web

---
+++ Caso2/WEB/Caso2/Controllers/CasasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Caso2.Controllers
+{
+    public class CasasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Caso2/WEB/Caso2/Models/CasasModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Caso2.Models
+{
+    public class CasasModel
+    {
+    }
+}
